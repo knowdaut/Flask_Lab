981a52f3318672453f17180330867ff67309acb2
knowdaut <nickdaut975@gmail.com>
2021-09-19 16:34:55 +0000

    test

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+ 
+                                             # import flask framework
+app = Flask(__name__)             # create an app instance
+
+@app.route("/")                
+def hello():                    
+    return render_template("index.html") 
+
+@app.route("/about")
+def about():
+    name = request.args.get('name') if request.args.get('name') else "Nicholas Daut!" 
+    return render_template("about.html", aboutName=name) 
+
+@app.route("/contact")                
+def contact():                    
+    return render_template("contact.html") 
+
+
+@app.route("/<name>")              # route with URL variable /<name>
+def hello_name(name):              # call method hello_name
+    return "Hello "+ name          # which returns "hello + name  
+
+
+if __name__ == "__main__":        # when running python app.py
+    app.run(debug=True)                     # run the flask app
